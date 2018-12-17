5920863f5e25798a22c95dd0a445b536b03102e4
Aidan Holland <thehappydinoa@gmail.com>
2018-12-17 14:39:00 -0500

    AutoDndGesturesSettingsActivity

--- common/install.sh
+++ common/install.sh
@@ -21,6 +21,7 @@ WELLBEING_PREF_FILE=$INSTALLER/common/PhenotypePrefs.xml
 chmod 660 $WELLBEING_PREF_FILE
 WELLBEING_PREF_FOLDER=/data/data/com.google.android.apps.wellbeing/shared_prefs/
 if [ -f $WELLBEING_PREF_FOLDER ]; then
+  pm enable "com.google.android.apps.wellbeing/com.google.android.apps.wellbeing.autodnd.ui.AutoDndGesturesSettingsActivity"
   cp -p $WELLBEING_PREF_FILE $WELLBEING_PREF_FOLDER
   am force-stop "com.google.android.apps.wellbeing"
 fi
