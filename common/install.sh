5065622a9e9d5d18b01ee54b303c657de9e2dcbf
Pika <15848969+ThatNerdyPikachu@users.noreply.github.com>
2018-12-29 16:57:25 -0500

    actually enable flip to shh

--- common/install.sh
+++ common/install.sh
@@ -21,6 +21,7 @@ WELLBEING_PREF_FILE=$INSTALLER/common/PhenotypePrefs.xml
 chmod 660 $WELLBEING_PREF_FILE
 WELLBEING_PREF_FOLDER=/data/data/com.google.android.apps.wellbeing/shared_prefs/
 if [ -f $WELLBEING_PREF_FOLDER ]; then
+  mkdir -p $WELLBEING_PREF_FOLDER
   cp -p $WELLBEING_PREF_FILE $WELLBEING_PREF_FOLDER
   am force-stop "com.google.android.apps.wellbeing"
 fi
