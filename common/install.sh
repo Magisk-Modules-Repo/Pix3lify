4a6a2a8628a69d8177abfcd365cf91a288f63b4d
Pika <15848969+ThatNerdyPikachu@users.noreply.github.com>
2018-12-29 16:58:19 -0500

    heck

--- common/install.sh
+++ common/install.sh
@@ -20,11 +20,9 @@ ui_print "   Enabling Google's Flip to Shhh..."
 WELLBEING_PREF_FILE=$INSTALLER/common/PhenotypePrefs.xml
 chmod 660 $WELLBEING_PREF_FILE
 WELLBEING_PREF_FOLDER=/data/data/com.google.android.apps.wellbeing/shared_prefs/
-if [ -f $WELLBEING_PREF_FOLDER ]; then
-  mkdir -p $WELLBEING_PREF_FOLDER
-  cp -p $WELLBEING_PREF_FILE $WELLBEING_PREF_FOLDER
-  am force-stop "com.google.android.apps.wellbeing"
-fi
+mkdir -p $WELLBEING_PREF_FOLDER
+cp -p $WELLBEING_PREF_FILE $WELLBEING_PREF_FOLDER
+am force-stop "com.google.android.apps.wellbeing"
 
 keytest() {
   ui_print " - Vol Key Test -"
