6bf56614faa30ba71089d5ad4671a0937c7cde22
JohnFawkes <33303104+JohnFawkes@users.noreply.github.com>
2019-01-01 17:31:59 -0500

    Update sepolicy.sh

--- common/sepolicy.sh
+++ common/sepolicy.sh
@@ -1,2 +1,3 @@
 # Put sepolicy statements here
 # Example: allow { audioserver mediaserver } audioserver_tmpfs file { read write open }
+create system_server sdcardfs file allow system_server sdcardfs file { write } 
