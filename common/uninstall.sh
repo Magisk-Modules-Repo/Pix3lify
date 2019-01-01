d8f48bf2d0e3b785bac83c3a48476d316d9a120f
Laster K <officiallazerl0rd@gmail.com>
2019-01-01 23:23:56 +0000

    Update uninstall.sh

--- common/uninstall.sh
+++ common/uninstall.sh
@@ -13,7 +13,7 @@ if [ -f $DIALER_PREF_FILE ]; then
   sed -i -e 's/name="__data_rollout__SpeakEasy.CallScreenOnPixelTwoRollout__launched__" value="true"/name="__data_rollout__SpeakEasy.CallScreenOnPixelTwoRollout__launched__" value="false"/g' $DIALER_PREF_FILE
   sed -i -e 's/name="G__speakeasy_postcall_survey_enabled" value="false"/name="G__speakeasy_postcall_survey_enabled" value="true"/g' $DIALER_PREF_FILE
   if $BOOTMODE; then
-  am force-stop "com.google.android.dialer"
+    am force-stop "com.google.android.dialer"
   fi
 fi
 
@@ -24,7 +24,7 @@ WELLBEING_PREF_FILE=/data/data/com.google.android.apps.wellbeing/shared_prefs/Ph
 if [ -f $WELLBEING_PREF_FILE ]; then
   rm -f $WELLBEING_PREF_FILE
   if $BOOTMODE; then
-  am force-stop "com.google.android.apps.wellbeing"
+    am force-stop "com.google.android.apps.wellbeing"
   fi
 fi
 
