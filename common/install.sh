6fbab6f31d53d7124a77a6a4ba2ef3435110b738
Laster K <officiallazerl0rd@gmail.com>
2019-01-04 15:04:26 +0000

    better volume options?
    fixed how sed should be under accent too
    the prop ro.boot.vendor.overlay.theme is ONLY related to the accent ($INSTALLER/system/vendor/overlay/Pixel)

--- common/install.sh
+++ common/install.sh
@@ -39,14 +39,12 @@ chooseold() {
   fi
 }
 
-SLIM=false; FULL=false; OVER=false; BOOT=false; ACC=false;
+SLIM=false; FULL=false; THEME=true;
 # Gets stock/limit from zip name
 case $(basename $ZIP) in
   *slim*|*Slim*|*SLIM*) SLIM=true;;
   *full*|*Full*|*FULL*) FULL=true;;
-  *over*|*Over*|*OVER*) OVER=true;;
-  *boot*|*Boot*|*BOOT*) BOOT=true;;
-  *acc*|*Acc*|*ACC*) ACC=true;;
+  *theme*|*Theme*|*THEME*) THEM=false;;
 esac
 
 # Keycheck binary by someone755 @Github, idea for code below by Zappo @xda-developers
@@ -81,7 +79,7 @@ if [ -f "$OVERLAY" ]; then
   rm -f "$OVERLAY"
 fi
 
-if [ "$SLIM" == false -a "$FULL" == false -a "$OVER" == false -a "$BOOT" ]; then
+if [ "$SLIM" == false -a "$FULL" == false -a "$THEM" == true ]; then
   if keytest; then
     FUNCTION=choose
   else
@@ -95,7 +93,7 @@ if [ "$SLIM" == false -a "$FULL" == false -a "$OVER" == false -a "$BOOT" ]; then
     $FUNCTION "DOWN"
   fi
 
-  if ! $SLIM && ! $FULL && ! $OVER && ! $BOOT && ! $ACC; then
+  if ! $SLIM && ! $FULL && $THEM; then
     ui_print " "
     ui_print " - Slim Options -"
     ui_print "   Do you want to enable slim mode (heavily reduced featureset, see README)?"
@@ -104,32 +102,16 @@ if [ "$SLIM" == false -a "$FULL" == false -a "$OVER" == false -a "$BOOT" ]; then
       SLIM=true
     else
       FULL=true
-    fi
-    if $FULL; then
       ui_print " "
       ui_print " - Overlay Options -"
-      ui_print "   Do you want the Pixel overlays enabled?"
+      ui_print "   Do you want the Google theme enabled?"
       ui_print "   Vol Up = Yes, Vol Down = No"
-      if $FUNCTION; then
-        OVER=true
-        ui_print " "
-        ui_print " - Accent Options -"
-        ui_print "   Do you want the Pixel accent enabled?"
-        ui_print "   Vol Up = Yes, Vol Down = No"
-        if $FUNCTION; then
-          ACC=true
-        fi
+      if ! THEM; then
+        THEM=false
       fi
     fi
-    ui_print " "
-    ui_print " - Animation Options -"
-    ui_print "   Do you want the Pixel boot animation?"
-    ui_print "   Vol Up = Yes, Vol Down = No"
-    if $FUNCTION; then
-      BOOT=true
-    fi
   else
-    ui_print " Options specified in zip name!"
+    ui_print "   Options specified in zip name!"
   fi
 fi
 
@@ -149,47 +131,24 @@ if $SLIM; then
   rm -rf $INSTALLER/system/vendor/overlay/DisplayCutoutNoCutout
   rm -rf $INSTALLER/system/vendor/overlay/Pixel
   rm -rf /data/resource-cache
-  rm -rf /data/dalvik-cache
-  ui_print "   Dalvik-Cache has been cleared!"
-  ui_print "   Next boot may take a little longer!"
 fi
 
 if $FULL; then
   ui_print " "
-  ui_print " Full mode selected..."
+  ui_print "   Full mode selected..."
   prop_process $INSTALLER/common/full.prop
-  if $OVER; then
+  if ! $THEM; then
     ui_print " "
-    ui_print "   Enabling overlay features..."
-  else
-    ui_print " "
-    ui_print "   Disabling overlay features..."
+    ui_print "   Disabling Google theme..."
     sed -i 's/ro.boot.vendor.overlay.theme/# ro.boot.vendor.overlay.theme/g' $INSTALLER/common/system.prop
-    rm -f $INSTALLER/system/vendor/overlay/Pix3lify.apk
-    rm -rf /data/resource-cache
-    rm -rf /data/dalvik-cache
-    ui_print "   Dalvik-Cache has been cleared!"
-    ui_print "   Next boot may take a little longer to boot!"
-  fi
-  if $ACC; then
-    ui_print " "
-    ui_print "   Enabling Pixel accent..."
-  else
-    ui_print " "
-    ui_print "   Disabling Pixel accent..."
+    sed -i 's/ro.config.ringtone/# ro.config.ringtone/g' $INSTALLER/common/system.prop
+    sed -i 's/ro.config.notification_sound/# ro.config.notification_sound/g' $INSTALLER/common/system.prop
+    sed -i 's/ro.config.alarm_alert/# ro.config.alarm_alert/g' $INSTALLER/common/system.prop
+    rm -rf $INSTALLER/system/fonts
+    rm -rf $INSTALLER/system/media
     rm -rf $INSTALLER/system/vendor/overlay/Pixel
     rm -rf /data/resource-cache
   fi
-fi
-
-if $BOOT; then
-  ui_print " "
-  ui_print "   Enabling boot animation..."
-  ui_print "   $UNITY$BFOLDER$BZIP"
-  cp -f $INSTALLER/common/bootanimation.zip $UNITY$BFOLDERBZIP
-else
-  ui_print " "
-  ui_print "   Disabling boot animation..."
 fi
 
 if [ $API -ge 27 ]; then
@@ -199,7 +158,7 @@ fi
 if [ $API -ge 28 ]; then
   ui_print " "
   ui_print "   Enabling Google's Call Screening..."
-  if [ "$SLIM" = false ]; then
+  if [ "$SLIM" == false ]; then
     ui_print " "
     ui_print "   Enabling Google's Flip to Shhh..."
     ui_print " "
