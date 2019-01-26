8c72c9b6263ebcb95a3fe08179362b22f21db5c3
JohnFawkes <33303104+JohnFawkes@users.noreply.github.com>
2019-01-26 18:03:32 -0500

    remove quotes

--- common/service.sh
+++ common/service.sh
@@ -1,7 +1,7 @@
 # This script will be executed in late_start service mode
 # More info in the main Magisk thread
 
-if [ $API -ge 28 ] && [ "$FULL" ]; then
+if [ $API -ge 28 ] && [ $FULL ]; then
   pm enable "com.google.android.apps.wellbeing/com.google.android.apps.wellbeing.autodnd.ui.AutoDndGesturesSettingsActivity"
   am force-stop "com.google.android.apps.wellbeing"
   if [[ $(pm list packages "com.google.android.soundpicker") ]]; then
