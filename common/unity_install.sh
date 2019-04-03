ec38a4226dd63888f97b044772bd5e709e16a3e8
JohnFawkes <33303104+JohnFawkes@users.noreply.github.com>
2019-04-02 23:56:33 -0400

    fixes
    Signed-off-by: JohnFawkes <33303104+JohnFawkes@users.noreply.github.com>

--- common/unity_install.sh
+++ common/unity_install.sh
@@ -55,22 +55,32 @@ patch_xml() {
   done
 }
 
-# Gets stock/limit from zip name
-SLIM=false; FULL=false; OVER=false BOOT=false; ACC=false; FONT=false
 OIFS=$IFS; IFS=\|
 case $(echo $(basename $ZIPFILE) | tr '[:upper:]' '[:lower:]') in
-  *slim*|*Slim*|*SLIM*) SLIM=true;;
-  *full*|*Full*|*FULL*) FULL=true;;
-  *over*|*Over*|*OVER*) OVER=true;;
-  *boot*|*Boot*|*BOOT*) BOOT=true;;
-  *acc*|*Acc*|*ACC*) ACC=true;;
-  *font*|*Font*|*FONT*) FONT=true;;
+  *full*) FULL=true;;
+  *slim*) FULL=false;;
+esac
+case $(echo $(basename $ZIPFILE) | tr '[:upper:]' '[:lower:]') in
+  *over*) OVER=true;;
+  *nover*) OVER=false;;
+esac
+case $(echo $(basename $ZIPFILE) | tr '[:upper:]' '[:lower:]') in
+  *font*) FONT=true;;
+  *nfont*) FONT=false;;
+esac
+case $(echo $(basename $ZIPFILE) | tr '[:upper:]' '[:lower:]') in
+  *acc*) ACC=true;;
+  *nacc*) ACC=false;;
+esac
+case $(echo $(basename $ZIPFILE) | tr '[:upper:]' '[:lower:]') in
+  *boot*) BOOT=true;;
+  *nboot*) BOOT=false;;
 esac
 IFS=$OIFS
 
 ## Debug Stuff
 log_start
-log_print "- Installing Logging Scripts/Prepping Terminal Script "
+ui_print "- Installing Logging Scripts/Prepping Terminal Script "
 cp_ch -n $TMPDIR/pix3lify.sh $UNITY/system/bin/pix3lify
 
 sed -i "s|<CACHELOC>|$CACHELOC|" $UNITY/system/bin/pix3lify
@@ -105,17 +115,19 @@ if [ -f "$OVERLAY" ]; then
   rm -f "$OVERLAY"
 fi
 
-if "$SLIM"  == false -a "$FULL" == false -a "$OVER" == false -a "$BOOT" == false -a "$ACC" == false ]; then
+if [ -z $FULL ] || [ -z $OVER ] || [ -z $FONT ] || [ -z $ACC ] || [ -z $BOOT ]; then
+  if [ -z $FULL ]; then
     ui_print " "
     log_print " - Slim Options -"
     log_print "   Do you want to enable slim mode (heavily reduced featureset, see README)?"
     log_print "   Vol Up = Yes, Vol Down = No"
-  if $VKSEL; then
-    SLIM=true >> $INSTLOG 2>&1
-  else
-    FULL=true >> $INSTLOG 2>&1
+    if $VKSEL; then
+      FULL=false >> $INSTLOG 2>&1
+    else
+      FULL=true >> $INSTLOG 2>&1
+    fi
   fi
-  if "$FULL"; then
+  if $FULL && ([ -z $OVER ] || [ -z $FONT ] || [ -z $ACC ] || [ -z $BOOT ]); then
     ui_print " "
     log_print " - Font Options -"
     log_print "   Do you want to replace fonts with Product Sans?"
@@ -125,13 +137,29 @@ if "$SLIM"  == false -a "$FULL" == false -a "$OVER" == false -a "$BOOT" == false
     else 
       FONT=false >> $INSTLOG 2>&1
     fi
-    if "$OOS"; then
-      log_print "   Pix3lify overlay has been known to not work and cause issues on devices running OxygenOS!"
-      log_print "   DO YOU WANT TO IGNORE OUR WARNINGS AND RISK A BOOTLOOP?"
-      log_print "   Vol Up = Yes, Vol Down = No"
-      if $VKSEL; then
-        ui_print " "
-        log_print "   Ignoring warnings..."
+    if [ -z $OVER ]; then
+      if [ "$OOS" ]; then
+        log_print "   Pix3lify overlay has been known to not work and cause issues on devices running OxygenOS!"
+        log_print "   DO YOU WANT TO IGNORE OUR WARNINGS AND RISK A BOOTLOOP?"
+        log_print "   Vol Up = Yes, Vol Down = No"
+        if $VKSEL; then
+          ui_print " "
+          log_print "   Ignoring warnings..."
+          ui_print " "
+          log_print " - Framework Options -"
+          log_print "   Do you want the Pixel framework enabled?"
+          log_print "   Vol Up = Yes, Vol Down = No"
+          if $VKSEL; then
+            OVER=true >> $INSTLOG 2>&1
+          else
+            OVER=false >> $INSTLOG 2>&1
+          fi
+        else
+          ui_print " "
+          log_print "  Disabling Overlay to prevent bootloop"
+          OVER=false
+        fi
+      else
         ui_print " "
         log_print " - Framework Options -"
         log_print "   Do you want the Pixel framework enabled?"
@@ -141,109 +169,95 @@ if "$SLIM"  == false -a "$FULL" == false -a "$OVER" == false -a "$BOOT" == false
         else
           OVER=false >> $INSTLOG 2>&1
         fi
-      else
-        ui_print " "
-        log_print "  Disabling Overlay to prevent bootloop"
-        OVER=false
       fi
-    else
-      ui_print " "
-      log_print " - Framework Options -"
-      log_print "   Do you want the Pixel framework enabled?"
+    fi
+    if [ -z $ACC ]; then
+      log_print " - Accent Options -"
+      log_print "   Do you want the Pixel accent enabled?"
       log_print "   Vol Up = Yes, Vol Down = No"
       if $VKSEL; then
-        OVER=true >> $INSTLOG 2>&1
+        ACC=true >> $INSTLOG 2>&1
       else
-        OVER=false >> $INSTLOG 2>&1
+        ACC=false >> $INSTLOG 2>&1
       fi
     fi
-    ui_print " "
-    log_print " - Accent Options -"
-    log_print "   Do you want the Pixel accent enabled?"
-    log_print "   Vol Up = Yes, Vol Down = No"
-    if $VKSEL; then
-      ACC=true >> $INSTLOG 2>&1
-    else
-      ACC=false >> $INSTLOG 2>&1
-    fi
-    ui_print " "
-    log_print " - Animation Options -"
-    log_print "   Do you want the Pixel boot animation?"
-    log_print "   Vol Up = Yes, Vol Down = No"
-    if $VKSEL; then
-      BOOT=true >> $INSTLOG 2>&1
-    else
-      BOOT=false >> $INSTLOG 2>&1
+    if [ -z $BOOT ]; then  
+      ui_print " "
+      log_print " - Animation Options -"
+      log_print "   Do you want the Pixel boot animation?"
+      log_print "   Vol Up = Yes, Vol Down = No"
+      if $VKSEL; then
+        BOOT=true >> $INSTLOG 2>&1
+      else
+        BOOT=false >> $INSTLOG 2>&1
+      fi
     fi
   fi
 else
-  log_print " Options specified in zip name!"
+  ui_print " Options specified in zip name!"
 fi
 
 if [ ! -f "/system/bin/curl" ]; then
   cp_ch $TMPDIR/curl $UNITY/system/bin/curl
 fi
 
-# had to break up volume options this way for basename zip for users without working vol keys
-if $SLIM; then
-  ui_print " "
-  log_print "   Enabling slim mode..."
-  sed -ri "s/name=(.*)/name=\1 (SLIM)/" $TMPDIR/module.prop
-  rm -rf $TMPDIR/system/app >> $INSTLOG 2>&1
-  rm -rf $TMPDIR/system/fonts >> $INSTLOG 2>&1
-  rm -rf $TMPDIR/system/lib >> $INSTLOG 2>&1
-  rm -rf $TMPDIR/system/lib64 >> $INSTLOG 2>&1
-  rm -rf $TMPDIR/system/media >> $INSTLOG 2>&1
-  rm -rf $TMPDIR/system/priv-app >> $INSTLOG 2>&1
-  rm -rf $TMPDIR/system/vendor/overlay/DisplayCutoutEmulationCorner >> $INSTLOG 2>&1
-  rm -rf $TMPDIR/system/vendor/overlay/DisplayCutoutEmulationDouble >> $INSTLOG 2>&1
-  rm -rf $TMPDIR/system/vendor/overlay/DisplayCutoutEmulationTall >> $INSTLOG 2>&1
-  rm -rf $TMPDIR/system/vendor/overlay/DisplayCutoutNoCutout >> $INSTLOG 2>&1
-  rm -rf $TMPDIR/system/vendor/overlay/Pixel >> $INSTLOG 2>&1
-  rm -rf /data/resource-cache >> $INSTLOG 2>&1
-fi
-
 if $FULL; then
   ui_print " "
-  log_print " Full mode selected..."
+  ui_print " Full mode selected..."
   sed -ri "s/name=(.*)/name=\1 (FULL)/" $TMPDIR/module.prop
   prop_process $TMPDIR/common/full.prop
   if $OVER; then
     ui_print " "
-    log_print "   Enabling Pixel framework..."
+    ui_print "   Enabling Pixel framework..."
   else
     ui_print " "
-    log_print "   Disabling Pixel framework..."
-    rm -f $TMPDIR/system/vendor/overlay/Pix3lify.apk >> $INSTLOG 2>&1
-    rm -rf /data/resource-cache >> $INSTLOG 2>&1
-    rm -rf /data/dalvik-cache >> $INSTLOG 2>&1
-    log_print "   Dalvik-Cache has been cleared!"
-    log_print "   Next boot may take a little longer to boot!"
+    ui_print "   Disabling Pixel framework..."
+    rm -f $TMPDIR/system/vendor/overlay/Pix3lify.apk
+    rm -rf /data/resource-cache
+    rm -rf /data/dalvik-cache
+    ui_print "   Dalvik-Cache has been cleared!"
+    ui_print "   Next boot may take a little longer to boot!"
   fi
   if $ACC; then
     ui_print " "
-    log_print "   Enabling Pixel accent..."
+    ui_print "   Enabling Pixel accent..."
   else
     ui_print " "
-    log_print "   Disabling Pixel accent..."
+    ui_print "   Disabling Pixel accent..."
     sed -i 's/ro.boot.vendor.overlay.theme/# ro.boot.vendor.overlay.theme/g' $TMPDIR/common/system.prop
-    rm -rf $TMPDIR/system/vendor/overlay/Pixel >> $INSTLOG 2>&1
-    rm -rf /data/resource-cache >> $INSTLOG 2>&1
+    rm -rf $TMPDIR/system/vendor/overlay/Pixel
+    rm -rf /data/resource-cache
   fi
+else
+  ui_print " "
+  ui_print "   Enabling slim mode..."
+  sed -ri "s/name=(.*)/name=\1 (SLIM)/" $TMPDIR/module.prop
+  rm -rf $TMPDIR/system/app
+  rm -rf $TMPDIR/system/fonts
+  rm -rf $TMPDIR/system/lib
+  rm -rf $TMPDIR/system/lib64
+  rm -rf $TMPDIR/system/media
+  rm -rf $TMPDIR/system/priv-app
+  rm -rf $TMPDIR/system/vendor/overlay/DisplayCutoutEmulationCorner
+  rm -rf $TMPDIR/system/vendor/overlay/DisplayCutoutEmulationDouble
+  rm -rf $TMPDIR/system/vendor/overlay/DisplayCutoutEmulationTall
+  rm -rf $TMPDIR/system/vendor/overlay/DisplayCutoutNoCutout
+  rm -rf $TMPDIR/system/vendor/overlay/Pixel
+  rm -rf /data/resource-cache
 fi
 
 if $BOOT; then
   ui_print " "
-  log_print "   Enabling boot animation..."
+  ui_print "   Enabling boot animation..."
   cp_ch -i $TMPDIR/common/bootanimation.zip $UNITY$BFOLDER$BZIP
 else
   ui_print " "
-  log_print "   Disabling boot animation..."
+  ui_print "   Disabling boot animation..."
 fi
 
 if $FONT; then
   ui_print " "
-  log_print "   Enabling fonts replacement..."
+  ui_print "   Enabling fonts replacement..."
   cp -rf /system/etc/fonts.xml $TMPDIR/system/etc/fonts.xml
   for i in $(find $TMPDIR/system/fonts/GoogleSans-* | sed 's|.*-||'); do
     sed -i "s|Roboto-$i|GoogleSans-$i|" $TMPDIR/system/etc/fonts.xml
@@ -253,7 +267,7 @@ if $FONT; then
   done
   fontls() {
     ui_print " "
-    log_print "   Replacing LockScreen Font.."
+    ui_print "   Replacing LockScreen Font.."
   }
   if [[ ! -e /system/fonts/AndroidClock.ttf ]]; then
     for j in /system/fonts/Clock*; do
@@ -263,29 +277,29 @@ if $FONT; then
   fi
 else
   ui_print " "
-  log_print "   Disabling fonts replacement..."
-  rm -rf $TMPDIR/system/etc/fonts.xml >> $INSTLOG 2>&1
+  ui_print "   Disabling fonts replacement..."
+  rm -rf $TMPDIR/system/etc/fonts.xml
 fi
 
 if [ $API -ge 27 ]; then
-  rm -rf $TMPDIR/system/framework  >> $INSTLOG 2>&1
+  rm -rf $TMPDIR/system/framework 
 fi
 
 if [ $API -ge 28 ]; then
   ui_print " "
-  log_print "   Enabling Google's Call Screening..."
+  ui_print "   Enabling Google's Call Screening..."
   DPF=$(find /data/data/com.google.android.dialer* -name "dialer_phenotype_flags.xml")
   if [ -f $DPF ]; then
     # Enabling Google's Call Screening
-    patch_xml -s $DPF '/map/boolean[@name="G__speak_easy_bypass_locale_check"]' "true" >> $INSTLOG 2>&1
-    patch_xml -s $DPF '/map/boolean[@name="G__speak_easy_enable_listen_in_button"]' "true" >> $INSTLOG 2>&1
-    patch_xml -s $DPF '/map/boolean[@name="__data_rollout__SpeakEasy.OverrideUSLocaleCheckRollout__launched__"]' "true" >> $INSTLOG 2>&1
-    patch_xml -s $DPF '/map/boolean[@name="G__enable_speakeasy_details"]' "true" >> $INSTLOG 2>&1
-    patch_xml -s $DPF '/map/boolean[@name="G__speak_easy_enabled"]' "true" >> $INSTLOG 2>&1
-    patch_xml -s $DPF '/map/boolean[@name="G__speakeasy_show_privacy_tour"]' "true" >> $INSTLOG 2>&1
-    patch_xml -s $DPF '/map/boolean[@name="__data_rollout__SpeakEasy.SpeakEasyDetailsRollout__launched__"]' "true" >> $INSTLOG 2>&1
-    patch_xml -s $DPF '/map/boolean[@name="__data_rollout__SpeakEasy.CallScreenOnPixelTwoRollout__launched__"]' "true" >> $INSTLOG 2>&1
-    patch_xml -s $DPF '/map/boolean[@name="G__speakeasy_postcall_survey_enabled"]' "true" >> $INSTLOG 2>&1
+    patch_xml -s $DPF '/map/boolean[@name="G__speak_easy_bypass_locale_check"]' "true"
+    patch_xml -s $DPF '/map/boolean[@name="G__speak_easy_enable_listen_in_button"]' "true"
+    patch_xml -s $DPF '/map/boolean[@name="__data_rollout__SpeakEasy.OverrideUSLocaleCheckRollout__launched__"]' "true"
+    patch_xml -s $DPF '/map/boolean[@name="G__enable_speakeasy_details"]' "true"
+    patch_xml -s $DPF '/map/boolean[@name="G__speak_easy_enabled"]' "true"
+    patch_xml -s $DPF '/map/boolean[@name="G__speakeasy_show_privacy_tour"]' "true"
+    patch_xml -s $DPF '/map/boolean[@name="__data_rollout__SpeakEasy.SpeakEasyDetailsRollout__launched__"]' "true"
+    patch_xml -s $DPF '/map/boolean[@name="__data_rollout__SpeakEasy.CallScreenOnPixelTwoRollout__launched__"]' "true"
+    patch_xml -s $DPF '/map/boolean[@name="G__speakeasy_postcall_survey_enabled"]' "true"
   fi
 fi
 
@@ -299,8 +313,8 @@ else
    rm -rf $TMPDIR/system/app/MarkupGoogle
 fi
 
-# Adds slim & full variables to service.sh
-for i in "SLIM" "FULL"; do
+# Adds full variables to service.sh
+for i in "FULL"; do
   sed -i "2i $i=$(eval echo \$$i)" $TMPDIR/common/service.sh
 done
 
@@ -308,6 +322,6 @@ cp_ch -i $UF/tools/$ARCH32/xmlstarlet $TMPDIR/system/bin/xmlstarlet
 
 cp_ch -i $UNITY/system/bin/pix3lify $UNITY/pix3lify
 
-log_print " If you encounter any bugs or issues, please type pix3lify"
-log_print " in a terminal emulator and choose yes to send logs to our server"
-log_print " WE DO NOT COLLECT ANY PERSONAL INFORMATION"
+ui_print " If you encounter any bugs or issues, please type pix3lify"
+ui_print " in a terminal emulator and choose yes to send logs to our server"
+ui_print " WE DO NOT COLLECT ANY PERSONAL INFORMATION"
